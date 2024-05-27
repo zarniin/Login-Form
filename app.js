6817a5dedd3681328e433b6350a95ec865296c2f
Arch Er <rzarni179206@gmail.com>
2024-05-27 13:24:50 +0630

    WDF batch 9 javascript

---
+++ app.js
@@ -0,0 +1,47 @@
+//Get UI
+
+var getmodalbtn = document.getElementById('modal-btn'),
+    getmodalbox = document.getElementById('login-modal'),
+    getbtnclose = document.querySelector('.btn-close');
+
+    
+//    getmodalbtn.addEventListener('click',function(){
+//        getmodalbox.style.display = "block";
+//        console.log('hay');
+//    });
+
+//    getbtnclose.addEventListener('click',function(){
+//        getmodalbox.style.display = "none";
+//    });
+       
+      getmodalbtn.addEventListener('click',openmodal);
+      getbtnclose.addEventListener('click',closemodal);
+
+      function openmodal(){
+        getmodalbox.style.display = "block";
+      }
+
+      function closemodal(){
+        getmodalbox.style.display = "none";
+      }
+
+      window.onclick = function(e){
+        //console.log(e.target);
+
+        if(e.target === getmodalbox){
+            closemodal();
+        }
+
+      }
+
+
+
+
+
+
+        
+
+
+
+
+
